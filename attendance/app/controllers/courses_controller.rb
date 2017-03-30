c1e37f5a5814fb1915031eb4ddbab152f70963e0
Deise Koreeda <deise.koreeda@gmail.com>
2017-03-30 09:38:16 -0400

    add course

--- attendance/app/controllers/courses_controller.rb
+++ attendance/app/controllers/courses_controller.rb
@@ -1,2 +1,39 @@
 class CoursesController < ApplicationController
+
+  def index
+    headers ={
+      "key" => "AIzaSyClwYiKGAt5Ia23N0EK8trzEZ_L-8oYAgk"
+    }
+
+    response = HTTParty.get(
+          "https://maps.googleapis.com/maps/api/place/textsearch/json?query=shillington+school+in+new+york&key=AIzaSyClwYiKGAt5Ia23N0EK8trzEZ_L-8oYAgk").parsed_response
+          # :headers => headers
+          # ).parsed_response
+
+    results = response["results"]
+
+    results.each do |result|
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+        flash[:notice] = "Course created successfully!"
+        redirect_back fallback_location: course_path
+    else
+        flash[:alert] = "Make sure you fill in all fields"
+        redirect_back fallback_location: new_course_path
+    end
+end
+
+private
+
+def course_params
+    params.require(:course).permit(:name, :school, :description, :teacher_id, :lat, :lng)
+end
+
 end
