668f762559b8d26057c3f2a2053426b78f5cfd48
Jackie Casper <Casper.Jaclyn@gmail.com>
2017-03-30 09:12:45 -0400

    add start date and end date to course

--- attendance/config/routes.rb
+++ attendance/config/routes.rb
@@ -21,11 +21,10 @@ Rails.application.routes.draw do
 
   get "/class_dates/new", to: "class_dates#new", as: :new_class_date
   post "/class_dates", to: "class_dates#create"
-  get "/class_dates/:id/edit", to: "class_dates#edit", as: :edit_class_date
   patch "/class_dates/:id", to: "class_dates#update"
   delete "/class_dates/:id", to: "class_dates#destroy", as: :delete_class_date
 
-  post "/attenders", to: "attenders#new", as: :check_in
+  post "/attenders", to: "attenders#create", as: :check_in
 
   get "/courses/:id/students", to: "students#index", as: :students
   get "/student/:id", to: "student#show", as: :student
--- attendance/db/schema.rb
+++ attendance/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329182849) do
+ActiveRecord::Schema.define(version: 20170330130912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20170329182849) do
     t.decimal  "lng"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.index ["teacher_id"], name: "index_courses_on_teacher_id", using: :btree
   end
 
