8a650ff0db7d45c2028d5a974dcedabfafcb0527
Deise Koreeda <deise.koreeda@gmail.com>
2017-04-01 21:01:59 -0400

    fix register feature

--- attendance/app/assets/javascripts/courses.js
+++ attendance/app/assets/javascripts/courses.js
@@ -4,9 +4,15 @@ $(()=>{
   const coursesNames = [];
   const courses = [];
   const coursesId = [];
+  const enrolledCourses = [];
+
   const searchCourse = $('#course_name');
 
+  $('.enrolled-course').each((i, course) => {
+    enrolledCourses.push($(course).attr('data-course-id'));
+  });
 
+  console.log(enrolledCourses);
   searchCourse.keyup((e) => {
       const search = searchCourse.val();
       // console.log("keyup");
@@ -15,7 +21,7 @@ $(()=>{
 
   function findCourse(e, search){
     //empty results before doing a new course search
-    // $('.results').empty();
+    $('.results').empty();
     // console.log("find", e);
     // console.log("search", search);
     const data = {
@@ -37,36 +43,37 @@ $(()=>{
   }
 
   const dropDownOptions = (courses) => {
-  console.log("courses", courses);
+    console.log("courses", courses);
 
-  $('.results').empty();
+    $('.results').empty();
 
-  for (let i = 0; i< courses.length; i++) {
-    console.log("hey");
-    courseOptions[i] = $('<div class="option">');
-    console.log("option1", courseOptions[i]);
-    coursesNames[i] = courses[i].name;
-    const name = $('<p>').text(coursesNames[i]);
-    courseOptions[i].append(name);
-    coursesId[i] = courses[i].id;
-    $('.results').append(courseOptions[i]);
-    courseOptions[i].click(() => {
-      console.log("click");
-      const courseSelected = $('#course_name').val(coursesNames[i]);
-      const studentId = $('#student_id').val();
-      $link = $('<a>',{
-        href: '/students/'+studentId+'/courses/'+coursesId[i]+'/register'
-      })
-      .text('register')
-      .appendTo($('.btn'));
-    })
+    for (let i = 0; i< courses.length; i++) {
+      console.log(courses[i].id);
+      if(!enrolledCourses.includes(courses[i].id.toString())) {
+        // console.log("hey");
+        courseOptions[i] = $('<div class="option">');
+        // console.log("option1", courseOptions[i]);
+        coursesNames[i] = courses[i].name;
+        const name = $('<p>').text(coursesNames[i]);
+        courseOptions[i].append(name);
+        coursesId[i] = courses[i].id;
+        $('.results').append(courseOptions[i]);
+        courseOptions[i].click(() => {
+          // console.log("click");
+          const courseSelected = $('#course_name').val(coursesNames[i]);
+          const studentId = $('#student_id').val();
+          registerCourse(studentId, coursesId[i]);
+          });
+      }
     }
   }
 
-
-
-
-
-
+  const registerCourse = (studentId, coursesId) => {
+      $link = $('<a>',{
+        href: '/students/'+studentId+'/courses/'+coursesId+'/register'
+      })
+      .text('register')
+      .appendTo($('.btn'));
+    }
 
 });
