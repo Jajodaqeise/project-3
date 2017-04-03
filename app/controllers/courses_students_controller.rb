c0ec779c7628c1b38bb8ca2915dd3e0c7787f4e0
Deise Koreeda <deise.koreeda@gmail.com>
2017-04-03 13:21:44 -0400

    fix register

--- app/controllers/courses_students_controller.rb
+++ app/controllers/courses_students_controller.rb
@@ -7,9 +7,13 @@ class CoursesStudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:student_id])
+    # puts " student #{@student} "
     @course = Course.find(params[:id])
+    # puts "course #{@course}"
+    # buybug
 
     @student_courses = @student.courses
+    # puts "student courses #{@student.courses}"
 
     course = []
     @student_courses.each do |course|
@@ -19,16 +23,23 @@ class CoursesStudentsController < ApplicationController
     end
 
     if course.length > 0
+      puts "hey"
       flash[:alert] = "You are already registered in this course"
       redirect_back fallback_location: courses_path
     else
-      @student.courses << @course
-      if @student.save
-          flash[:notice] = "Student registered successfully!"
-          redirect_to course_path @course
+      if @student.courses.include?(@course)
+        puts "course exists"
+        flash[:alert] = "You have already registered for this course"
+        redirect_back fallback_location: courses_path
       else
-          flash[:alert] = "Couldn't registered for this course"
-          redirect_back fallback_location: courses_path
+        @student.courses << @course
+        if @student.save
+            flash[:notice] = "Student registered successfully!"
+            redirect_to course_path @course
+        else
+            flash[:alert] = "Couldn't registered for this course"
+            redirect_back fallback_location: courses_path
+        end
       end
     end
   end
